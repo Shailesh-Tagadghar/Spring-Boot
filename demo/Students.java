5032b1d115de2b5619a8aaa78dcaf0e01db9d783
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-02 18:38:52 +0530

    Starting With Spring Boot

---
+++ demo/Students.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Students {
+	public Students() {
+		System.out.println("Student Object is Created");
+	}
+}
