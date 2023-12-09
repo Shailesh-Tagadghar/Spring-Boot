0d2f8c1f0a32dc2c0c6a33ec59ef1e5db55e80d2
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-09 18:51:15 +0530

    MyController.java
    Using @Service annotation in it as well

--- example/demo/MyController.java
+++ example/demo/MyController.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+	
+	@Autowired
+	private MyService myService;
 
 	@Value("${spring.application.name}")
 	private String appName;
@@ -15,5 +19,14 @@ public class MyController {
 		System.out.println(appName);
 		return "Welcome from "+appName;
 	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		myService.login();
+		
+		return "welcome to login page";
+	}
+
 	
 }
