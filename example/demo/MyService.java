ac884cbcc1977c588afa72ff9ab6e98119ad321b
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-09 18:51:48 +0530

    @Service Annotation File

---
+++ example/demo/MyService.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+	
+	public void login() {
+		System.out.println("Within service login");
+	}
+	
+}
