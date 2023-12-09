e2fe41298120688c2f22396609d86d77101fd30a
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-09 18:39:01 +0530

    Rest Controller for Rest

---
+++ example/demo/MyController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	@GetMapping("/home")
+	public String index() {
+		System.out.println(appName);
+		return "Welcome from "+appName;
+	}
+
+	
+}
---
+++ example/demo/PropertiesDemosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesDemosApplication.class, args);
+	}
+
+}
