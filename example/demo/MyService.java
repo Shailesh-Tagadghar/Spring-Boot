4e20e1ed9c989f729f1019a377e2f6a6672f4746
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-10 18:01:57 +0530

    MyService.java

--- example/demo/MyService.java
+++ example/demo/MyService.java
@@ -1,12 +1,36 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
 	
-	public void login() {
-		System.out.println("Within service login");
+//	public void login() {
+//		System.out.println("Within service login");
+//	}
+	
+	private Map<Integer,Employee> allemployees =
+			new HashMap<>();
+	
+	//insert 
+	public void addEmp(Employee employee) {
+		allemployees.put(employee.getId(),employee);
+		System.out.println(allemployees);
 	}
+	
+	//update 
+	public void updateEmp(Employee employee) {
+		allemployees.put(employee.getId(),employee);
+		System.out.println(allemployees);
+	}
+	
+	//delete
+	public void deleteEmp(int id) {
+		allemployees.remove(id);
+	}
+
 	
 }
