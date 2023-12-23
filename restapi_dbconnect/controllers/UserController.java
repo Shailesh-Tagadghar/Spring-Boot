006126e27767bf3f4905ae46558816a60c812bd1
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-23 17:19:25 +0530

    UserController.java
    In this code we write Crud Operation

---
+++ restapi_dbconnect/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.restapi_dbconnect.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restapi_dbconnect.models.User;
+import com.restapi_dbconnect.services.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/users")
+	public String adduser(@RequestBody User user) {
+		userService.addUser(user);
+		return "User SeccessFully Added ! ";
+	}
+	
+	@GetMapping("/users")
+	public Iterable<User> getAll(){
+		Iterable<User> allUsers = userService.getAll();
+		return allUsers;
+	}
+	
+	@GetMapping("/users/{id}")
+	public User getbyId() {
+		return null;
+	}
+}
