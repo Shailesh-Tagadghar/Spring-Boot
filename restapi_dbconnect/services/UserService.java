d95101dd980b486f8c165634a6d9e7857771905c
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-23 18:42:08 +0530

    UserService.java
    Whole Code for Service class

--- restapi_dbconnect/services/UserService.java
+++ restapi_dbconnect/services/UserService.java
@@ -1,7 +1,9 @@
 package com.restapi_dbconnect.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.restapi_dbconnect.models.User;
 import com.restapi_dbconnect.repositories.UserRepository;
@@ -20,20 +22,19 @@ public class UserService {
 		Iterable<User> allUsers = userRepository.findAll();
 		return allUsers;
 	}
-
+	
 	public User getById(int id) {
 		User user = userRepository.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Id"));
 		return user;
 	}
-
+	
 	public void deleteUser(int id) {
 		userRepository.deleteById(id);
 	}
-
+	
 	public User updateUser(User user) {
 		User updatedUser = userRepository.save(user);
 		return updatedUser;
 	}
-	
 }
