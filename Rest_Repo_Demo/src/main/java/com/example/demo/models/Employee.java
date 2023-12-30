1686368b0673aa088629dd30ed751f3a0e196cf3
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-30 17:28:07 +0530

    REST Repository with DB Connection

---
+++ Rest_Repo_Demo/src/main/java/com/example/demo/models/Employee.java
@@ -0,0 +1,68 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	// this models we use for create database and its schema and table, fields like that.
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name, dept;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private int salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+	
+	
+}
