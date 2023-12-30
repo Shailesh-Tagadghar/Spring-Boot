bedab777b642e08d40346ad5333e4766d7679687
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-30 18:47:43 +0530

    EmployeeReository.java
    Adding Projection code here to display the selected content

--- Rest_Repo_Demo/src/main/java/com/example/demo/repositories/EmployeeReository.java
+++ Rest_Repo_Demo/src/main/java/com/example/demo/repositories/EmployeeReository.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.models.Employee;
 
+
+@RepositoryRestResource(excerptProjection = EmployeeProjection.class)
 public interface EmployeeReository extends CrudRepository<Employee, Integer> {
 
 }
