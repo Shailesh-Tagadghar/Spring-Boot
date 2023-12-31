fe16e2a42c4a4a079f95de5de9206952b43a3cc3
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-31 14:19:09 +0530

    AddressRepository.java

--- RelationDemo's/one_to_one/AddressRepository.java
+++ RelationDemo's/one_to_one/AddressRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.one_to_one;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AddressRepository extends CrudRepository<Address, Integer> {
-
+//this is Address interface to create table into database and to interact with database 
 }
