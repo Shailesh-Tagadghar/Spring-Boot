8e5463f01748ee9e38b9c793f579034df488accc
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-31 15:15:50 +0530

    EmployeesRepository.java
    Adding @Projection Annotation into Employees-Repository 
    for reading the selected records data

--- RelationDemo's/one_to_one/EmployeesRepository.java
+++ RelationDemo's/one_to_one/EmployeesRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.one_to_one;
 
 import org.springframework.data.repository.CrudRepository;
 
+@RepositoryRestResource(excerptProjection = EmployeesProjection.class)
 public interface EmployeesRepository extends CrudRepository<Employees, Integer> {
 //this is employee interface to create table into database and to interact with database 
 }
