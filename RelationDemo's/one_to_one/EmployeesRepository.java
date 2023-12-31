afb1ae67fd0a8b67971c6468ce805bbd09feab20
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-12-31 14:18:47 +0530

    EmployeesRepository.java

--- RelationDemo's/one_to_one/EmployeesRepository.java
+++ RelationDemo's/one_to_one/EmployeesRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.one_to_one;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeesRepository extends CrudRepository<Employees, Integer> {
-
+//this is employee interface to create table into database and to interact with database 
 }
