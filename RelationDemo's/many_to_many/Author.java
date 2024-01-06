682ecdfaf3314051db7ceab1d4c06758a0f8d73a
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-06 17:14:04 +0530

    Author.java
    Created list to add foreign key to mapped the other id 
    by using Mapped

--- RelationDemo's/many_to_many/Author.java
+++ RelationDemo's/many_to_many/Author.java
@@ -1,9 +1,12 @@
 package com.example.demo.many_to_many;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -15,6 +18,17 @@ public class Author {
 	private String name;
 	
 	private String quali;
+	
+	@ManyToMany(mappedBy = "authors")
+	private List<Book> books;
+	
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
 
 	public int getAuthorId() {
 		return authorId;
@@ -39,4 +53,6 @@ public class Author {
 	public void setQuali(String quali) {
 		this.quali = quali;
 	}
+	
+	
 }
