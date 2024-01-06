ca809389a0e009af6b3c3163d96517668109f8e1
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-06 17:03:33 +0530

    Book.java
    Updated Foreign Key using @JoinTable annotation

--- RelationDemo's/many_to_many/Book.java
+++ RelationDemo's/many_to_many/Book.java
@@ -1,9 +1,14 @@
 package com.example.demo.many_to_many;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Book {
@@ -15,6 +20,21 @@ public class Book {
 	private String title;
 	
 	private int price;
+	
+	@ManyToMany
+	@JoinTable(
+			joinColumns = @JoinColumn(referencedColumnName = "bookId"),
+			inverseJoinColumns = @JoinColumn(referencedColumnName = "authorId")
+			)
+	private List<Author> authors;
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
 
 	public int getBookId() {
 		return bookId;
@@ -39,4 +59,5 @@ public class Book {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
 }
