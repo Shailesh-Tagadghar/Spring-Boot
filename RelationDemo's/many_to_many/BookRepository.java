8c78aabc47f6233e20be985e2b7a85de0668c801
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-06 17:27:48 +0530

    BookRepository.java
    To get Projection of that class

--- RelationDemo's/many_to_many/BookRepository.java
+++ RelationDemo's/many_to_many/BookRepository.java
@@ -2,5 +2,7 @@ package com.example.demo.many_to_many;
 
 import org.springframework.data.repository.CrudRepository;
 
+@RepositoryRestResource(excerptProjection = BookProjection.class)
 public interface BookRepository extends CrudRepository<Book, Integer> {
+  
 }
