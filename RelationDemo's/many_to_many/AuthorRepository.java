451a31e7edef904502cb06e8be2d40c77eb4409d
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-06 16:48:22 +0530

    AuthorRepository.java

--- RelationDemo's/many_to_many/AuthorRepository.java
+++ RelationDemo's/many_to_many/AuthorRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.many_to_many;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
-
+  
 }
