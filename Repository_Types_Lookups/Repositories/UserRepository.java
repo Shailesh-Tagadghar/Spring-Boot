b05766fb3f5e287c3bb4bcf97cb3df6927eab57d
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-07 16:48:34 +0530

    UserRepository.java
    New methods to search user's

--- Repository_Types_Lookups/Repositories/UserRepository.java
+++ Repository_Types_Lookups/Repositories/UserRepository.java
@@ -8,9 +8,19 @@ import com.example.demo.Models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	//custom search query methods - lookups
+	
+	//for single user email
 	public Optional<User> findByEmail(String email);
+	
+	//for multiple user name
+	public Iterable<User> findAllByName(String name);
+	
+	//using logical operators - AND || &&
+	public Optional<User> findByEmailAndPassword(String email, String password);
 	
 }
 
-
-// to search by email :- http://localhost:8080/users/search/findByEmail?email=sat@gmail.com
+//to search by email :- http://localhost:8080/users/search/findByEmail?email=sat@gmail.com
+//to search by name :- http://localhost:8080/users/search/findAllByName?name=sat
+//to search by AND logical operator :- http://localhost:8080/users/search/findByEmailAndPassword?email=sat@gmail.com&&password=sat@123
