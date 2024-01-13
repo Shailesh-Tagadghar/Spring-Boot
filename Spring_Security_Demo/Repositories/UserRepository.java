665f3ce2df6835792e7a84372ff7599d44d204bf
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-13 18:58:24 +0530

    UserRepository.java

--- Spring_Security_Demo/Repositories/UserRepository.java
+++ Spring_Security_Demo/Repositories/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.SecurityDemo.Models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+  @RestResource(exported = false)
+	public User save(User user);
   
 }
