04b1ee92604ef1dd562127630850fd6084cdc034
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-13 18:56:54 +0530

    UserDTO.java ---Data Transfer Object
    in this file we are going add @validation annotation

---
+++ Spring_Security_Demo/Models/UserDTO.java
@@ -0,0 +1,50 @@
+package com.example.SecurityDemo.Models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+//DTO -- Data Transfer Object
+
+public class UserDTO {
+	
+	@NotNull
+	private String name;
+	
+	@NotNull
+	@Email
+	private String email;
+	
+	@NotNull
+	@Pattern(regexp = "[a-zA-Z0-9@]*")
+	@Size(min = 8,max = 15)
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
