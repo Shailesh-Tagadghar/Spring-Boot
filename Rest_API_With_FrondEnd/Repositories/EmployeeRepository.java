299bfece59c1fcc89237b6cdc5aff7faef52847e
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-21 18:03:38 +0530

    EmployeeRepository.java

--- Rest_API_With_FrondEnd/Repositories/EmployeeRepository.java
+++ Rest_API_With_FrondEnd/Repositories/EmployeeRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.Models.Employee;
 
+@CrossOrigin(origins = "*",maxAge = 5000, allowCredentials = "false") //this will remove cors error in console
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+  
 }
