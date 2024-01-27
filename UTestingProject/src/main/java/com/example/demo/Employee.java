f186380071d01cb7839bbc8a2afb20fccfc57526
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-27 17:26:45 +0530

    Employee.java

---
+++ UTestingProject/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String dept;
+	
+	private int salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+}
