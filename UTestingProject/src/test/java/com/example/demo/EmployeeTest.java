d6dbbccc18b8738962b820bf6eb53eb979f43a6e
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-27 18:05:36 +0530

    Update EmployeeTest.java

--- UTestingProject/src/test/java/com/example/demo/EmployeeTest.java
+++ UTestingProject/src/test/java/com/example/demo/EmployeeTest.java
@@ -13,6 +13,8 @@ public class EmployeeTest {
 	private EmployeeRepository repository;
 	
 	@Test
+	//@Disabled //to skip this test case
+	@Order(1) // to set order of execution
 	public void addEmployeeTest() {
 		Employee e = new Employee();
 	     // this test case will be failed due to salary>=10000
@@ -25,7 +27,18 @@ public class EmployeeTest {
 		e.setDept("CS");
 		e.setSalary(12000);
 		Employee obj = repository.save(e);
-		
 		assertNotNull(obj);
+	}
+
+
+	@Test
+	@Order(2)
+	public void updateEmployee() {
+		Employee e = repository.findById(1).orElseThrow(()-> new NullPointerException("The test case is failed !!"));
+		e.setDept("Computer Science");
+		e.setSalary(26000);
+                Employee obj = repository.save(e);
+		assertNotNull(obj);
+				
 	}
 }
