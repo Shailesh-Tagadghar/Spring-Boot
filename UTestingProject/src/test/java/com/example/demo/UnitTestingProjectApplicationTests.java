502884d3035e3000f948da4a5bf267912a412737
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-27 17:14:35 +0530

    UnitTestingProjectApplicationTests.java

--- UTestingProject/src/test/java/com/example/demo/UnitTestingProjectApplicationTests.java
+++ UTestingProject/src/test/java/com/example/demo/UnitTestingProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+//import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class UnitTestingProjectApplicationTests {
 
 	@Test
+	//@Disabled //to disabled specific test case
 	void contextLoads() {
 		System.out.println("It says all clear go for development !!");
 	}
