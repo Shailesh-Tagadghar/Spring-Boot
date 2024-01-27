07ee4c6349bfc8d39f7537e993bbf96e9199754e
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-01-27 18:24:04 +0530

    Update EmployeeTest.java

--- UTestingProject/src/test/java/com/example/demo/EmployeeTest.java
+++ UTestingProject/src/test/java/com/example/demo/EmployeeTest.java
@@ -12,33 +12,57 @@ public class EmployeeTest {
 	@Autowired
 	private EmployeeRepository repository;
 	
+	//	@Test
+////	@Disabled //to skip this test case
+//	@Order(1) // to set order of execution
+//	public void addEmployeeTest() {
+//		Employee e = new Employee();
+//		e.setName("Amit");
+//		e.setDept("IT");
+//		e.setSalary(8000);
+//		Employee obj = repository.save(e);
+//		assertNotNull(obj);
+//	}
+	
 	@Test
-	//@Disabled //to skip this test case
-	@Order(1) // to set order of execution
-	public void addEmployeeTest() {
+	@Order(1)
+	public void addEmployeeTest1() {
 		Employee e = new Employee();
-	     // this test case will be failed due to salary>=10000
-	     //	e.setName("Amit");
-	     //	e.setDept("IT");
-	     // e.setSalary(8000);
-
-		//this test case will pass 
-		e.setName("Shailesh");
-		e.setDept("CS");
-		e.setSalary(12000);
+		e.setName("Radhika");
+		e.setDept("Shop");
+		e.setSalary(40000);
+		Employee obj = repository.save(e);
+		assertNotNull(obj);
+	}
+	
+	@Test
+	@Order(2)
+	public void addEmployeeTest2() {
+		Employee e = new Employee();
+		e.setName("Ashok");
+		e.setDept("Garment");
+		e.setSalary(25000);
 		Employee obj = repository.save(e);
 		assertNotNull(obj);
 	}
 
 
 	@Test
-	@Order(2)
+	@Order(3)
 	public void updateEmployee() {
 		Employee e = repository.findById(1).orElseThrow(()-> new NullPointerException("The test case is failed !!"));
 		e.setDept("Computer Science");
 		e.setSalary(26000);
-                Employee obj = repository.save(e);
-		assertNotNull(obj);
-				
+        Employee obj = repository.save(e);
+		assertNotNull(obj);		
+	}
+
+	@Test
+	@Order(4)
+	public void findAllTest() {
+		List<Employee> emp = (List<Employee>) repository.findAll();
+		int actualCount = emp.size();
+		int expectedCount = 3;
+		assertEquals(expectedCount, actualCount);
 	}
 }
